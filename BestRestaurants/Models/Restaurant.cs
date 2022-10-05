5dceae01480162702180b0ba26baeb710c61744d
Shaniza Lingle <shanizalingle@gmail.com>
2022-10-05 13:44:17 -0700

    WIP: fix mySQL
    Co-authored-by: Joseph Jackson <josephjacksonw@gmail.com>
    Co-authored-by: Tim Empey <timothyempey@gmail.com>

--- BestRestaurants/Models/Restaurant.cs
+++ BestRestaurants/Models/Restaurant.cs
@@ -3,7 +3,10 @@ namespace BestRestaurants.Models
   public class Restaurant
   {
     public int RestaurantId { get; set; }
-    public string Description { get; set; }
+    public string Name { get; set; } //was previously Description
+    public int Rating { get; set; } //new
+    public int Cost { get; set; } //new
+    public string Type { get; set; }//new
     public int CuisineId { get; set; }
     public virtual Cuisine Cuisine { get; set; }
   }
